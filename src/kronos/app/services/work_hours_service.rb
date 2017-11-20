73d2ad3660ad47dfb43cdf1a731c590fd45d9ec7
gyamin <n.gyamin@gmail.com>
2017-11-21 02:21:50 +0900

    #3 プロジェクト機能実装

--- src/kronos/app/services/work_hours_service.rb
+++ src/kronos/app/services/work_hours_service.rb
@@ -13,6 +13,9 @@ class WorkHoursService
     # 稼働時間
     db_work_hours = WorkedHoursDao.selectReportWorkedData(@params[:id])
 
+    # 稼働予定時間
+    db_planed_work_hours WorkedHoursDao.selectReportPlanedWorkData(@params[:id])
+
     tmp_work_hours = {}
     tmp_week_nums = []
 
@@ -43,6 +46,38 @@ class WorkHoursService
     data[:work_hours] = work_hours
 
     return data
+  end
+
+  private
+  def convert_row_report_hash(db_data)
+    tmp_hours = {}
+    tmp_week_nums = []
+
+    db_data.each do | elem |
+      workers_id = elem['workers_id']
+      week_num = elem['week_num_of_year']
+      tmp = {}
+      tmp[week_num] = elem['week_work_hours']
+
+      if(tmp_hours.key?(workers_id))
+        tmp_hours[workers_id].push(tmp)
+      else
+        tmp_hours[workers_id] = []
+        tmp_hours[workers_id].push(tmp)
+      end
+
+      # 週番号を配列で保持
+      tmp_week_nums.push(week_num)
+    end
+
+    hours = {}
+    hours[:worker_week_hours] = tmp_hours
+    tmp_week_nums.uniq!
+
+    hours[:start_week] = tmp_week_nums.min
+    hours[:end_week] = tmp_week_nums.max
+
+    return hours
   end
 
 end
