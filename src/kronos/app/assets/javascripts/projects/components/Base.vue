8c690944f73429db246dee546b49a0e87629555e
gyamin <n.gyamin@gmail.com>
2017-12-05 00:29:36 +0900

    #3 Vueの単一ファイルコンポーネントを利用した例を実装

--- src/kronos/app/assets/javascripts/projects/components/Base.vue
+++ src/kronos/app/assets/javascripts/projects/components/Base.vue
@@ -5,12 +5,36 @@
 </template>
 
 <script lang="ts">
+    import Logger from "./../../common/logger"
+    import * as $ from 'jquery';
     import Vue from "vue";
     export default {
         name: "base",
-        data() {
-            return {
-                greet : "こんにちわ"
+        data(this: any) {
+          return {
+              data: this.apiGetData.done((data: any) => {
+                  this.data = data
+              })
+          }
+        },
+        methods: {
+            apiGetData: () => {
+                let defer = $.Deferred();
+                let url = "/api/projects/1"
+                $.ajax({
+                    url: url,
+                    method: "GET"
+                })
+                    .done((data) => {
+                        Logger.info(url + " success to call api.");
+                        Logger.debug(data);
+                        defer.resolve(data);
+                    })
+                    .fail(() => {
+                        Logger.error(url + " failed to call api.");
+                        defer.reject;
+                    })
+                return defer.promise();
             }
         }
     }
