3365b6dea11d86f877f46d33718e824b04f23652
gyamin <n.gyamin@gmail.com>
2017-12-07 01:24:55 +0900

    #3 プロジェクト詳細画面 稼働時間表示

--- src/kronos/app/assets/javascripts/projects/components/progress.vue
+++ src/kronos/app/assets/javascripts/projects/components/progress.vue
@@ -1,20 +1,22 @@
 <template>
-    <table>
-        <thead>
-            <td class="worker_name">氏名</td>
-            <td v-for="(term, key) in progress.terms">
-                {{term.cweek}}
-            </td>
-        </thead>
+    <div class="progress">
+        <table>
+            <thead>
+                <td class="worker_name">氏名</td>
+                <td v-for="(term, key) in progress.terms">
+                    {{term.cweek}}
+                </td>
+            </thead>
 
-        <tr v-for="(elem, key) in progress.workHours">
-            <td class="worker_name">{{elem.family_name}}{{elem.first_name}}</td>
-            <td v-for="(hours, key) in elem.hours">
-                {{hours}}
-            </td>
-        </tr>
+            <tr v-for="(elem, key) in progress.workHours">
+                <td class="worker_name">{{elem.family_name}}{{elem.first_name}}</td>
+                <td v-for="(hours, key) in elem.hours">
+                    {{hours}}
+                </td>
+            </tr>
 
-    </table>
+        </table>
+    </div>
 </template>
 
 <script lang="ts">
@@ -26,6 +28,11 @@
 </script>
 
 <style scoped>
+    .progress {
+        width: 900px;
+        overflow-y:scroll;
+    }
+
     table {
         border-collapse: collapse;
         table-layout: fixed;
@@ -37,6 +44,7 @@
     table tr td {
         padding: 0 5px;
         border: 1px solid #DDD;
+        text-align: center;
         vertical-align: middle;
         width: 50px;
     }
@@ -46,7 +54,6 @@
     {
         width: 100px;
     }
-
 
     table tr th {
         text-align: center;
