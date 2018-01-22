b82d43a79be863e5e9e7c5207213c02d5601b263
Yasumasa Nogami <nogami@fenrir.co.jp>
2018-01-22 23:44:25 +0900

    #2 稼働予定登録時、日付処理修正

--- src/kronos/app/controllers/sales_controller.rb
+++ src/kronos/app/controllers/sales_controller.rb
@@ -1,13 +1,14 @@
 class SalesController < ApplicationController
   # 売上処理トップ画面
   def index
-    @title = '売上処理'
+    @title = '売上処理ホーム'
     render :action => 'index'
   end
 
   # 売上計上処理実施画面
   def show_sales_operation
-
+    @title = '売上計上処理実行'
+    render :action => 'show_sales_operation'
   end
 
   # 売上計上処理実行
